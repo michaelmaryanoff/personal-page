2fc991908a29ca32aee3b0c801828d38790c58c5
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-09-08 18:10:40 -0500

    Add more projects

--- src/components/pages/ProjectsPage.js
+++ src/components/pages/ProjectsPage.js
@@ -5,7 +5,8 @@ import ProjectCard from './projectsPageComponents/ProjectCard';
 import movieRouletteImage from '../../images/MovieRoulette.png';
 import bakingCalculatorImage from '../../images/BakingCalculator.png';
 import personalPageImage from '../../images/PersonalPage.png';
-import cofeeCalculator from '../../images/CoffeeCalc.jpeg';
+import cofeeCalculatorImage from '../../images/CoffeeCalc.jpeg';
+import movieRouletteSwiftImage from '../../images/MovieRouletteSwift.png';
 
 import LargeHeader from './projectsPageComponents/LargeHeader';
 
@@ -50,13 +51,24 @@ const ProjectsPage = () => {
       <div className="ui eight column stackable grid">
         <div className="ui three column center aligned centered row">
           <ProjectCard
-            imagePath={cofeeCalculator}
+            imagePath={cofeeCalculatorImage}
             alt="Simple coffee calculator"
             header="Simple coffee calculator"
             description="A simple calculator app that calculates coffee to water ratios."
             metaContent="Technologies used: Swift, UIKit"
             projectLink="https://apps.apple.com/us/app/simple-coffee-calculator/id1455234543"
             gitHubLink="https://github.com/michaelmaryanoff/Ultimate-Coffee-Calculator"
+          />
+          <ProjectCard
+            imagePath={movieRouletteSwiftImage}
+            alt="Movie Roulette (iOS)"
+            header="Movie Roulette (iOS)"
+            description={
+              'The iOS version of the "Movie Roulette" app. Note: this app is currently not featured on the app store, so you will need to run via readme instructions on github repo'
+            }
+            metaContent="Technologies used: Swift, UIKit, SwiftyJSON, TMDB Api"
+            projectLink="https://github.com/michaelmaryanoff/MovieRoulette"
+            gitHubLink="https://github.com/michaelmaryanoff/MovieRoulette"
           />
         </div>
       </div>
