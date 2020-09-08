f00e0483e65ae4818807f5311d90c5b6e588dbca
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-09-08 14:40:38 -0500

    Two cards with basic styling

--- src/components/HeroHeader.js
+++ src/components/HeroHeader.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import SocialLinks from './mainpage/SocialLinks';
+import SocialLinks from './mainPageComponents/SocialLinks';
 
 const HeroHeader = () => {
   return (
--- src/components/pages/ProjectsPage.js
+++ src/components/pages/ProjectsPage.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import ProjectCard from '../projectsPageComponents/ProjectCard';
+
+import movieRouletteImage from '../../images/MovieRoulette.png';
+
 const ProjectsPage = () => {
-  return <div>ProjectsPage</div>;
+  return (
+      <div className="ui basic segment">
+        <div className="ui eight column grid">
+          <div className="ui four column center aligned centered row">
+              <ProjectCard imagePath={movieRouletteImage} alt="Movie Roulette" header="Movie Roulette" description="An app which uses the tmdb Api to find a random movie to watch." metaContent="Technologies used: React, TMDB API" />
+              <ProjectCard imagePath={movieRouletteImage} alt="Baking Calculator"  header="High Altitude Baking Calculator" description="Allows you to bake at high altitudes." metaContent="Technologies used: React, Redux, React Router"/>
+            </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectsPage;
