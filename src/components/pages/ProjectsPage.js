b59dbd25a45f5bc42150fdac15cc59a6a2598969
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-09-09 16:52:40 -0500

    Create working projects page

--- src/components/pages/ProjectsPage.js
+++ src/components/pages/ProjectsPage.js
@@ -5,13 +5,15 @@ import ProjectCard from './projectsPageComponents/ProjectCard';
 import movieRouletteImage from '../../images/MovieRoulette.png';
 import bakingCalculatorImage from '../../images/BakingCalculator.png';
 import personalPageImage from '../../images/PersonalPage.png';
+import coffeeCalculatorImage from '../../images/CoffeeCalc.jpeg';
+import movieRouletteSwiftImage from '../../images/MovieRouletteSwift.png';
 
 import LargeHeader from './projectsPageComponents/LargeHeader';
 
 const ProjectsPage = () => {
   return (
     <div className="basic segment">
-      <div className="container">
+      <div className="ui container">
         <div className="ui centered center aligned stackable grid">
           <LargeHeader label="React Projects" />
           <div className="ui row">
@@ -34,6 +36,7 @@ const ProjectsPage = () => {
                 projectLink="https://highaltitudebaking-calculator.web.app"
                 gitHubLink="https://github.com/michaelmaryanoff/high-altitude-baking-calculator"
               />
+
               <ProjectCard
                 imagePath={personalPageImage}
                 alt="Personal Page"
@@ -42,6 +45,31 @@ const ProjectsPage = () => {
                 metaContent="Technologies used: React, React Router"
                 projectLink="https://highaltitudebaking-calculator.web.app"
                 gitHubLink="https://github.com/michaelmaryanoff/high-altitude-baking-calculator"
+              />
+            </div>
+          </div>
+          <LargeHeader label="Swift projects" />
+          <div className="ui row">
+            <div className="ui centered cards">
+              <ProjectCard
+                imagePath={coffeeCalculatorImage}
+                alt="Simple coffee calculator"
+                header="Simple coffee calculator"
+                description="A simple calculator app that calculates coffee to water ratios."
+                metaContent="Technologies used: Swift, UIKit"
+                projectLink="https://apps.apple.com/us/app/simple-coffee-calculator/id1455234543"
+                gitHubLink="https://github.com/michaelmaryanoff/Ultimate-Coffee-Calculator"
+              />
+              <ProjectCard
+                imagePath={movieRouletteSwiftImage}
+                alt="Movie Roulette (iOS)"
+                header="Movie Roulette (iOS)"
+                description={
+                  'The iOS version of the "Movie Roulette" app. Note: this app is currently not featured on the app store, so you will need to run via readme instructions on github repo'
+                }
+                metaContent="Technologies used: Swift, UIKit, SwiftyJSON, TMDB Api"
+                projectLink="https://github.com/michaelmaryanoff/MovieRoulette"
+                gitHubLink="https://github.com/michaelmaryanoff/MovieRoulette"
               />
             </div>
           </div>
